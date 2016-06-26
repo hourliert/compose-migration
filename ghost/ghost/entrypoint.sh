94a15f9abdc93eb5bbeb57931b84c76461fb09c5
Thomas Hourlier <thomas.hourlier@cnode.fr>
2016-06-26 21:31:20 +0200

    fix(ghost): persit data on host
    Wuick workaround: Created a new entrypoint based on original.
    Waiting for ghost/issues/36 to be resolved.

---
+++ ghost/ghost/entrypoint.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -e
+
+if [[ "$*" == npm*start* ]]; then
+	baseDir="$GHOST_SOURCE/content"
+	for dir in "$baseDir"/*/ "$baseDir"/themes/*/; do
+		targetDir="$GHOST_CONTENT/${dir#$baseDir/}"
+		mkdir -p "$targetDir"
+		if [ -z "$(ls -A "$targetDir")" ]; then
+			tar -c --one-file-system -C "$dir" . | tar xC "$targetDir"
+		fi
+	done
+
+  # check if a default config is here then copy it "$GHOST_CONTENT/config.js"
+  if [ -e "$GHOST_SOURCE/config.default.js" ]; then
+    cp "$GHOST_SOURCE/config.default.js" "$GHOST_CONTENT/config.js"
+	fi
+
+	if [ ! -e "$GHOST_CONTENT/config.js" ]; then
+		sed -r '
+			s/127\.0\.0\.1/0.0.0.0/g;
+			s!path.join\(__dirname, (.)/content!path.join(process.env.GHOST_CONTENT, \1!g;
+		' "$GHOST_SOURCE/config.example.js" > "$GHOST_CONTENT/config.js"
+	fi
+
+	ln -sf "$GHOST_CONTENT/config.js" "$GHOST_SOURCE/config.js"
+
+	chown -R user "$GHOST_CONTENT"
+
+	set -- gosu user "$@"
+fi
+
+exec "$@"
